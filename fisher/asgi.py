1051bbedda65374fd26c64128d3672aa945424c2
zmrenwu <zmrenwu@163.com>
2020-06-21 23:06:10 +0900

    修改 asgi 配置

--- fisher/asgi.py
+++ fisher/asgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
+import django
+from channels.routing import get_default_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fisher.settings.prod')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fisher.settings.prod")
 
-application = get_asgi_application()
+django.setup()
+application = get_default_application()
