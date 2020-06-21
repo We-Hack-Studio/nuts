835291bbdd3f90595167384e0e6ea52d8347818b
EC2 Default User <ec2-user@ip-172-31-46-74.ap-southeast-1.compute.internal>
2020-06-21 13:27:09 +0000

    修复依赖和uvicorn配置问题

--- run_uvicorn.py
+++ run_uvicorn.py
@@ -4,10 +4,10 @@ import uvicorn
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fisher.settings.prod")
     os.environ.setdefault("READ_DOT_ENV_FILE", "yes")
     uvicorn.run(
-        "config.asgi:application", host="0.0.0.0", port=7000,
+        "fisher.asgi:application", host="0.0.0.0", port=7000,
     )
 
 
