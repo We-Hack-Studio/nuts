08e689d1d54c368da61f598919b920e7068ccdfd
zmrenwu <zmrenwu@163.com>
2020-07-06 20:49:35 +0900

    Disable csrf check

--- config/settings/common.py
+++ config/settings/common.py
@@ -72,6 +72,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "core.middleware.DisableCSRFCheckMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
