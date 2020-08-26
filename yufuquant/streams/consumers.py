34b17ed449577fc75237c14bbd0fe9a4a0242224
yangxg <zmrenwu@163.com>
2020-08-26 20:47:10 +0900

    Add broadcast websocket api

--- yufuquant/streams/consumers.py
+++ yufuquant/streams/consumers.py
@@ -4,8 +4,10 @@ import re
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from users.models import User
+from django.contrib.auth.models import AnonymousUser
 
-public_topic_patterns = []
+public_topic_patterns = [
+]
 private_topic_patterns = [
     r"robot#\d+\.log",
     r"robot#\d+\.asset",
@@ -35,6 +37,7 @@ class StreamConsumer(AsyncJsonWebsocketConsumer):
         super().__init__(*args, **kwargs)
         self._topics = []
         self._event = asyncio.Event()
+        self.user = AnonymousUser()
 
     async def connect(self):
         await super().connect()
@@ -44,33 +47,36 @@ class StreamConsumer(AsyncJsonWebsocketConsumer):
         # todo: validate the content
         cmd = content["cmd"]
         if cmd == "auth":
-            token = content["params"]
+            token = content["token"]
             await self.auth(token)
         elif cmd == "sub":
-            topics = content["params"]
+            topics = content["topics"]
             await self.sub(topics)
         elif cmd == "unsub":
-            topics = content["params"]
+            topics = content["topics"]
             await self.unsub(topics)
+        elif cmd == "broadcast":
+            message = content["message"]
+            await self.broadcast(message)
         else:
             await self.send_json({"code": 405, "detail": f"不支持的指令 {cmd}"}, close=True)
 
     async def auth(self, token: str):
         # Already authenticated
-        if self.scope.get("user"):
+        if self.user.is_authenticated:
             return
 
         async_user_get = database_sync_to_async(User.objects.get)
         try:
             user = await async_user_get(auth_token__key=token)
-            self.scope["user"] = user
+            self.user = user
             await self.send_json({"code": 200, "detail": "认证成功"})
         except User.DoesNotExist:
             await self.send_json({"code": 400, "detail": "认证失败"}, close=True)
 
     async def sub(self, topics):
         public_topics, private_topics = _category_topics(topics)
-        if len(private_topics) > 0 and not self.scope.get("user"):
+        if len(private_topics) > 0 and not self.user.is_authenticated:
             await self.send_json({"code": 401, "detail": "包含认证后才可订阅的话题"}, close=True)
             return
 
@@ -96,6 +102,24 @@ class StreamConsumer(AsyncJsonWebsocketConsumer):
         except asyncio.TimeoutError:
             await self.send_json({"code": 400, "detail": "1分钟内未订阅任何话题"}, close=True)
 
-    async def robot_stream(self, event):
-        message = event["text"]
+    async def broadcast(self, message):
+        if self.user.is_anonymous:
+            await self.send_json({"code": 403, "detail": "认证后才可广播消息"}, close=True)
+            return
+
+        topic = message["topic"]
+        if topic == "robotPing":
+            pass
+        elif topic == "robotLog":
+            log_topic_groups = [g for g in self.groups if g.endswith("log")]
+            for group in log_topic_groups:
+                await self.channel_layer.group_send(group, {
+                    'type': 'robot.log',
+                    'message': message
+                })
+        else:
+            pass
+
+    async def robot_log(self, event):
+        message = event["message"]
         await self.send_json(message)
