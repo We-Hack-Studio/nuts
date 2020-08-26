06411dd942661a577a109ca586bd99c5208eddbc
yangxg <zmrenwu@163.com>
2020-08-26 12:37:42 +0900

    Implement websocket api

--- yufuquant/streams/consumers.py
+++ yufuquant/streams/consumers.py
@@ -1,37 +1,100 @@
-import json
-from datetime import datetime
+import asyncio
+import re
 
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
-from core.utils import KeyHelper
-from urllib.parse import parse_qs
+from users.models import User
+
+public_topic_patterns = []
+private_topic_patterns = [
+    r"robot#\d+\.log",
+    r"robot#\d+\.asset",
+    r"robot#\d+\.order",
+    r"robot#\d+\.strategyParameters",
+]
+topic_patterns = public_topic_patterns + private_topic_patterns
 
 
-class PrivateStreamConsumer(AsyncJsonWebsocketConsumer):
+def _category_topics(topics):
+    public = []
+    private = []
+    for topic in topics:
+        if not any(re.match(pattern, topic) for pattern in topic_patterns):
+            continue
+
+        if any(re.match(pattern, topic) for pattern in public_topic_patterns):
+            public.append(topic)
+        elif any(re.match(pattern, topic) for pattern in private_topic_patterns):
+            private.append(topic)
+
+    return public, private
+
+
+class StreamConsumer(AsyncJsonWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._topics = []
+        self._event = asyncio.Event()
+
+    async def connect(self):
+        await super().connect()
+        asyncio.get_event_loop().create_task(self.ensure_sub())
+
     async def receive_json(self, content, **kwargs):
-        params = content.get("params")
-        cmd = content.get("cmd")
-        key = content.get("key")
-        if not key:
-            await self.close()
+        # todo: validate the content
+        cmd = content["cmd"]
+        if cmd == "auth":
+            token = content["params"]
+            await self.auth(token)
+        elif cmd == "sub":
+            topics = content["params"]
+            await self.sub(topics)
+        elif cmd == "unsub":
+            topics = content["params"]
+            await self.unsub(topics)
+        else:
+            await self.send_json({"code": 405, "detail": f"不支持的指令 {cmd}"}, close=True)
 
-        data = KeyHelper.from_key(key=key)
-        if not data:
-            await self.close()
+    async def auth(self, token: str):
+        # Already authenticated
+        if self.scope.get("user"):
+            return
 
-        group_name = data["channel_group_name"]
-        if cmd == "sub":
-            for group_name in params:
-                await self.channel_layer.group_add(
-                    group_name.lower(), self.channel_name
-                )
-                self.groups.append(group_name.lower())
+        async_user_get = database_sync_to_async(User.objects.get)
+        try:
+            user = await async_user_get(auth_token__key=token)
+            self.scope["user"] = user
+            await self.send_json({"code": 200, "detail": "认证成功"})
+        except User.DoesNotExist:
+            await self.send_json({"code": 400, "detail": "认证失败"}, close=True)
 
-        if cmd == "unsub":
-            for group_name in params:
-                await self.channel_layer.group_discard(
-                    group_name.lower(), self.channel_name
-                )
-                self.groups.remove(group_name.lower())
+    async def sub(self, topics):
+        public_topics, private_topics = _category_topics(topics)
+        if len(private_topics) > 0 and not self.scope.get("user"):
+            await self.send_json({"code": 401, "detail": "包含认证后才可订阅的话题"}, close=True)
+            return
+
+        for topic in public_topics + private_topics:
+            group_name = topic.replace("#", "-")
+            await self.channel_layer.group_add(group_name, self.channel_name)
+            self.groups.append(group_name)
+            self._topics.append(topic)
+
+        if len(self._topics) > 0:
+            self._event.set()
+
+    async def unsub(self, topics):
+        for topic in topics:
+            group_name = topic.replace("#", "-")
+            await self.channel_layer.group_discard(group_name, self.channel_name)
+            self.groups.remove(group_name)
+            self._topics.remove(topic)
+
+    async def ensure_sub(self):
+        try:
+            await asyncio.wait_for(self._event.wait(), timeout=30)
+        except asyncio.TimeoutError:
+            await self.send_json({"code": 400, "detail": "1分钟内未订阅任何话题"}, close=True)
 
     async def robot_stream(self, event):
         message = event["text"]
---
+++ yufuquant/streams/tests.py
@@ -0,0 +1,88 @@
+import pytest
+from channels.testing import WebsocketCommunicator
+
+from .consumers import StreamConsumer
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_auth_with_bad_token():
+    communicator = WebsocketCommunicator(StreamConsumer, "/ws/streams/")
+    connected, subprotocol = await communicator.connect()
+    assert connected
+    await communicator.send_json_to(data={"cmd": "auth", "params": "bad-auth-token"})
+    response = await communicator.receive_json_from()
+    assert response["code"] == 400
+    # Close
+    await communicator.disconnect()
+
+
+# Here transaction=True is necessary: https://github.com/django/channels/issues/1110
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_auth_with_valid_token(admin_user):
+    communicator = WebsocketCommunicator(StreamConsumer, "/ws/streams/")
+    connected, subprotocol = await communicator.connect()
+    assert connected
+    await communicator.send_json_to(data={"cmd": "auth", "params": admin_user.auth_token.key})
+    response = await communicator.receive_json_from()
+    assert response["code"] == 200
+    assert communicator.scope["user"] == admin_user
+    # Close
+    await communicator.disconnect()
+
+
+# https://github.com/django/channels/issues/1343
+@pytest.mark.skip
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_already_auth(admin_user):
+    communicator = WebsocketCommunicator(StreamConsumer, "/ws/streams/")
+    connected, subprotocol = await communicator.connect()
+    assert connected
+    await communicator.send_json_to(data={"cmd": "auth", "params": admin_user.auth_token.key})
+    response = await communicator.receive_json_from()
+    assert response["code"] == 200
+    assert communicator.scope["user"] == admin_user
+
+    await communicator.send_json_to(data={"cmd": "auth", "params": admin_user.auth_token.key})
+    with pytest.raises(AssertionError):
+        await communicator.receive_from()
+
+    # Close
+    await communicator.disconnect()
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_sub_private_topics_without_auth():
+    communicator = WebsocketCommunicator(StreamConsumer, "/ws/streams/")
+    connected, subprotocol = await communicator.connect()
+    assert connected
+    await communicator.send_json_to(data={"cmd": "sub", "params": ["robot#1.log"]})
+    response = await communicator.receive_json_from()
+    assert response["code"] == 401
+
+    # Close
+    await communicator.disconnect()
+
+
+@pytest.mark.skip
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_sub_private_topics_with_auth(admin_user):
+    communicator = WebsocketCommunicator(StreamConsumer, "/ws/streams/")
+    connected, subprotocol = await communicator.connect()
+    assert connected
+
+    await communicator.send_json_to(data={"cmd": "auth", "params": admin_user.auth_token.key})
+    response = await communicator.receive_json_from()
+    assert response["code"] == 200
+    assert communicator.scope["user"] == admin_user
+
+    await communicator.send_json_to(data={"cmd": "sub", "params": ["robot#1.log"]})
+    with pytest.raises(AssertionError):
+        await communicator.receive_from()
+
+    # Close
+    await communicator.disconnect()
