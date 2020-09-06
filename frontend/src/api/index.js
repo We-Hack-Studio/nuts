3f9ef05d5e6cc722417259f98848f94d664df88d
laomaner <1050397167@qq.com>
2020-09-06 21:59:01 +0900

    YUFU-49,51,57,58

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,53 +1,57 @@
-import {annonInstance, authInstance} from "@/axiosService"
+import { annonInstance, authInstance } from '@/axiosService';
 
 export async function postAuthLogin(data) {
-    return annonInstance.post("/auth/login/?include=user", data)
+  return annonInstance.post('/auth/login/?include=user', data);
 }
 
 export async function getUsersMe() {
-    return authInstance.get("/users/me/");
+  return authInstance.get('/users/me/');
 }
 
 export async function getExchanges() {
-    return annonInstance.get("/exchanges/");
+  return annonInstance.get('/exchanges/');
 }
 
 export async function getCredentials() {
-    return await authInstance.get("/credentials/?include=exchange");
+  return await authInstance.get('/credentials/?include=exchange');
 }
 
 export async function postCredentials(data) {
-    return authInstance.post("/credentials/", data);
+  return authInstance.post('/credentials/', data);
 }
 
 export async function deleteCredentialsId(credId) {
-    return authInstance.delete("/credentials/" + credId + "/");
+  return authInstance.delete('/credentials/' + credId + '/');
 }
 
 export async function postRobots(data) {
-    return authInstance.post("/robots/", data);
+  return authInstance.post('/robots/', data);
 }
 
 export async function patchRobotsId(robotId, data) {
-    return authInstance.patch("/robots/" + robotId + "/", data);
+  return authInstance.patch('/robots/' + robotId + '/', data);
+}
+
+export async function deleteRobotsId(robotId) {
+  return authInstance.delete('/robots/' + robotId + '/');
 }
 
 export async function getRobotsId(robotId) {
-    return authInstance.get("/robots/" + robotId + "/");
+  return authInstance.get('/robots/' + robotId + '/');
 }
 
 export async function getRobots() {
-    return authInstance.get("/robots/");
+  return authInstance.get('/robots/');
 }
 
 export async function getStrategies() {
-    return authInstance.get("/strategies/");
+  return authInstance.get('/strategies/');
 }
 
 export async function postStrategies(data) {
-    return authInstance.post("/strategies/", data);
+  return authInstance.post('/strategies/', data);
 }
 
 export async function postRobotsIdStrategyParameters(robotId, data) {
-    return authInstance.post(`/robots/${robotId}/strategyParameters/`, data);
+  return authInstance.post(`/robots/${robotId}/strategyParameters/`, data);
 }
--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -3,15 +3,32 @@
     <div>
       <b-navbar toggleable="lg" type="light" variant="light">
         <b-container>
-          <b-navbar-toggle target="nav-collapse" class="mr-auto"></b-navbar-toggle>
-          <b-dropdown variant="link" right class="d-md-none">
+          <b-navbar-toggle
+            target="nav-collapse"
+            class="mr-auto no-outline"
+          ></b-navbar-toggle>
+          <b-dropdown
+            variant="link"
+            right
+            class="d-md-none"
+            toggle-class="no-shadow"
+          >
             <template v-slot:button-content>
               <b-icon-plus></b-icon-plus>
             </template>
-            <b-dropdown-item :to="{name: 'robot-create'}">机器人</b-dropdown-item>
-            <b-dropdown-item :to="{name: 'strategy-create'}">策略</b-dropdown-item>
+            <b-dropdown-item :to="{ name: 'robot-create' }"
+              >机器人</b-dropdown-item
+            >
+            <b-dropdown-item :to="{ name: 'strategy-create' }"
+              >策略</b-dropdown-item
+            >
           </b-dropdown>
-          <b-dropdown variant="link" right class="d-md-none">
+          <b-dropdown
+            variant="link"
+            right
+            class="d-md-none"
+            toggle-class="pr-0 no-shadow"
+          >
             <template v-slot:button-content>
               <b-avatar size="sm"></b-avatar>
             </template>
@@ -24,8 +41,8 @@
               <b-icon-x-diamond scale="1.5"></b-icon-x-diamond>
             </b-navbar-brand>
             <b-navbar-nav class="mr-auto" v-if="isAuthenticated">
-              <b-nav-item :to="{name: 'robot-list'}">机器人</b-nav-item>
-              <b-nav-item :to="{name: 'strategy-list'}">策略</b-nav-item>
+              <b-nav-item :to="{ name: 'robot-list' }">机器人</b-nav-item>
+              <b-nav-item :to="{ name: 'strategy-list' }">策略</b-nav-item>
               <b-nav-item to="/connect">接入</b-nav-item>
             </b-navbar-nav>
 
@@ -34,21 +51,31 @@
                 <template v-slot:button-content>
                   <b-icon-plus></b-icon-plus>
                 </template>
-                <b-dropdown-item :to="{name: 'robot-create'}">机器人</b-dropdown-item>
-                <b-dropdown-item :to="{name: 'strategy-create'}">策略</b-dropdown-item>
+                <b-dropdown-item :to="{ name: 'robot-create' }"
+                  >机器人</b-dropdown-item
+                >
+                <b-dropdown-item :to="{ name: 'strategy-create' }"
+                  >策略</b-dropdown-item
+                >
               </b-dropdown>
-              <b-dropdown variant="link" right class="d-none d-md-block" toggle-class="pr-0">
+              <b-dropdown
+                variant="link"
+                right
+                class="d-none d-md-block"
+                toggle-class="pr-0"
+              >
                 <template v-slot:button-content>
                   <b-avatar size="sm"></b-avatar>
                 </template>
                 <b-dropdown-item to="/account">账户</b-dropdown-item>
-                <b-dropdown-item to="/login" @click="logout">登出</b-dropdown-item>
+                <b-dropdown-item to="/login" @click="logout"
+                  >登出</b-dropdown-item
+                >
               </b-dropdown>
             </template>
             <b-navbar-nav v-else class="ml-auto">
               <b-nav-item to="/login">登录</b-nav-item>
             </b-navbar-nav>
-
           </b-collapse>
         </b-container>
       </b-navbar>
@@ -57,24 +84,30 @@
 </template>
 
 <script>
-import {mapGetters} from 'vuex'
+import { mapGetters } from "vuex";
 
 export default {
   name: "NavBar",
   methods: {
-    logout: function () {
-      this.$store.commit("REMOVE_AUTH_TOKEN")
-      this.$store.commit("REMOVE_USER")
-    }
+    logout: function() {
+      this.$store.commit("REMOVE_AUTH_TOKEN");
+      this.$store.commit("REMOVE_USER");
+    },
   },
   computed: {
-    ...mapGetters([
-      'isAuthenticated',
-    ])
-  }
-}
+    ...mapGetters(["isAuthenticated"]),
+  },
+};
 </script>
 
 <style scoped>
-
+/deep/ .no-shadow.btn-link {
+  color: #212529;
+}
+/deep/ .no-shadow:focus {
+  box-shadow: none;
+}
+/deep/ .no-outline:focus {
+  outline: none;
+}
 </style>
