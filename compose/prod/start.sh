fb36f646cfe5e54c96ed197866b9934b41f85d61
yangxg <zmrenwu@163.com>
2020-09-07 22:31:03 +0900

    compile messages before start

--- compose/prod/start.sh
+++ compose/prod/start.sh
@@ -2,4 +2,5 @@
 
 python manage.py migrate
 python manage.py collectstatic --noinput
+python manage.py compilemessages
 gunicorn config.asgi:application -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 --chdir=/app
