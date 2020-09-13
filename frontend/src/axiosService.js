44baa2dcaefed1f8e66479928a23daddf968e6e5
laomaner <1050397167@qq.com>
2020-09-13 14:39:22 +0900

    feat: YUFU-61 Direct unauthenticated user to login page 401 全局拦截

--- frontend/src/axiosService.js
+++ frontend/src/axiosService.js
@@ -1,25 +1,44 @@
-import axios from 'axios'
-import store from "./store";
+import axios from 'axios';
+import store from './store';
+import storage from './utils';
 
 export const annonInstance = axios.create({
-    baseURL: window.conf.restApiBaseUrl,
-    timeout: 10000,
+  baseURL: window.conf.restApiBaseUrl,
+  timeout: 10000,
 });
-annonInstance.interceptors.request.use(function (config) {
-    config.headers["Content-Type"] = "application/vnd.api+json"
-    config.headers["Accept"] = "application/vnd.api+json"
-    return config;
+annonInstance.interceptors.request.use(function(config) {
+  config.headers['Content-Type'] = 'application/vnd.api+json';
+  config.headers['Accept'] = 'application/vnd.api+json';
+  return config;
 });
 
 export const authInstance = axios.create({
-    baseURL: window.conf.restApiBaseUrl,
-    timeout: 10000,
+  baseURL: window.conf.restApiBaseUrl,
+  timeout: 10000,
 });
-authInstance.interceptors.request.use(function (config) {
-    config.headers["Content-Type"] = "application/vnd.api+json"
-    config.headers["Accept"] = "application/vnd.api+json"
-    if (store.state.authToken) {
-        config.headers["Authorization"] = "Token " + store.state.authToken
+
+authInstance.interceptors.request.use(function(config) {
+  config.headers['Content-Type'] = 'application/vnd.api+json';
+  config.headers['Accept'] = 'application/vnd.api+json';
+  if (store.state.authToken) {
+    config.headers['Authorization'] = 'Token ' + store.state.authToken;
+  }
+  return config;
+});
+
+authInstance.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    if (error.response.status === 401) {
+      if (window.location.pathname !== '/login') {
+        window.location.href = '/#/login';
+        return null;
+      }
+      storage.clear();
     }
-    return config;
-});
+
+    return Promise.reject(error);
+  },
+);
