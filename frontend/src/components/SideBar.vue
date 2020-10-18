15f52d2781088446879691b4b7b28dc755b09bd1
laomaner <1050397167@qq.com>
2020-10-18 22:20:52 +0900

    step3. 该路由

--- frontend/src/components/SideBar.vue
+++ frontend/src/components/SideBar.vue
@@ -1,84 +1,85 @@
 <template>
-  <!-- Sidebar -->
-    <ul :class="['navbar-nav', 'bg-gradient-primary', 'sidebar', 
-       'sidebar-dark', 'accordion', isToggled?'toggled':'']" id="accordionSidebar">
-
-      <!-- Sidebar - Brand -->
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon">
-          <b-icon icon="x-diamond"></b-icon>
-        </div>
-        <div class="sidebar-brand-text mx-3">YUFU 量化</div>
-      </a>
-
-      <!-- Divider -->
-      <hr class="sidebar-divider my-0">
-
-      <!-- Nav Item - Dashboard -->
-      <li class="nav-item active">
-        <a class="nav-link" href="index.html">
-          <b-icon icon="cpu" class="mr-2"></b-icon>
-          <span>机器人</span>
+    <!-- Sidebar -->
+    <ul
+        :class="[
+            'navbar-nav',
+            'bg-gradient-primary',
+            'sidebar',
+            'sidebar-dark',
+            'accordion',
+            isToggled ? 'toggled' : '',
+        ]"
+        id="accordionSidebar"
+    >
+        <!-- Sidebar - Brand -->
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+            <div class="sidebar-brand-icon">
+                <b-icon icon="x-diamond"></b-icon>
+            </div>
+            <div class="sidebar-brand-text mx-3">YUFU 量化</div>
         </a>
-      </li>
 
-      <!-- Divider -->
-      <!-- <hr class="sidebar-divider"> -->
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0" />
 
-      <!-- Heading -->
-      <!-- <div class="sidebar-heading">
+        <!-- Nav Item - Dashboard -->
+        <b-nav-item class="nav-link" :to="{ name: 'robot-list' }">
+            <b-icon icon="cpu" class="mr-2"></b-icon>
+            <span>机器人</span>
+        </b-nav-item>
+
+        <!-- Divider -->
+        <!-- <hr class="sidebar-divider"> -->
+
+        <!-- Heading -->
+        <!-- <div class="sidebar-heading">
         策略
       </div> -->
 
-      <!-- Nav Item - Pages Collapse Menu -->
-      <li class="nav-item">
-        <a class="nav-link" href="#" >
-          <b-icon icon="graph-up" class="mr-2"></b-icon>
-          <span>策略</span>
-        </a>
-      </li>
+        <!-- Nav Item - Pages Collapse Menu -->
+        <b-nav-item class="nav-link" :to="{ name: 'strategy-list' }">
+            <b-icon icon="graph-up" class="mr-2"></b-icon>
+            <span>策略</span>
+        </b-nav-item>
 
-   
+        <!-- Divider -->
+        <!-- <hr class="sidebar-divider"> -->
 
-      <!-- Divider -->
-      <!-- <hr class="sidebar-divider"> -->
-
-      <!-- Heading -->
-      <!-- <div class="sidebar-heading">
+        <!-- Heading -->
+        <!-- <div class="sidebar-heading">
         接入
       </div> -->
 
-      <!-- Nav Item - Charts -->
-      <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-          <b-icon icon="key" class="mr-2"></b-icon>
-          <span>接入</span></a>
-      </li>
+        <!-- Nav Item - Charts -->
+        <b-nav-item class="nav-link" to="/connect">
+            <b-icon icon="key" class="mr-2"></b-icon>
+            <span>接入</span>
+        </b-nav-item>
+        <!-- <router-link to="/connect">接入</router-link> -->
 
-      <!-- Divider -->
-      <!-- <hr class="sidebar-divider d-none d-md-block"> -->
+        <!-- Divider -->
+        <!-- <hr class="sidebar-divider d-none d-md-block"> -->
 
-      <!-- Sidebar Toggler (Sidebar) -->
-      <div class="text-center d-none d-md-inline">
-        <!-- <button class="rounded-circle border-0" id="sidebarToggle" @click="sidebarToggle"></button> -->
-        <b-button class="rounded-circle border-0 mt-3" id="sidebarToggle" @click="sidebarToggle" >
-          <b-icon :icon="isToggled? 'chevron-right':'chevron-left'" class="toggle-icon" ></b-icon>
-        </b-button>
-      </div>
-
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+            <!-- <button class="rounded-circle border-0" id="sidebarToggle" @click="sidebarToggle"></button> -->
+            <b-button class="rounded-circle border-0 mt-3" id="sidebarToggle" @click="sidebarToggle">
+                <b-icon :icon="isToggled ? 'chevron-right' : 'chevron-left'" class="toggle-icon"></b-icon>
+            </b-button>
+        </div>
     </ul>
     <!-- End of Sidebar -->
 </template>
 
 <script>
 export default {
-  props:['isToggled'],
-  methods: {
-    sidebarToggle() {
-      this.$emit('toggleSideBar')
-    }
-  }
-}
+    props: ['isToggled'],
+    methods: {
+        sidebarToggle() {
+            this.$emit('toggleSideBar');
+        },
+    },
+};
 </script>
 
 <style lang="scss">
--- frontend/src/components/TopBar.vue
+++ frontend/src/components/TopBar.vue
@@ -1,81 +1,44 @@
 <template>
-     <!-- Topbar -->
-        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-          <!-- Sidebar Toggle (Topbar) -->
-          <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3" @click="toggleSideBar">
+    <!-- Topbar -->
+    <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+        <!-- Sidebar Toggle (Topbar) -->
+        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3" @click="toggleSideBar">
             <b-icon icon="justify"></b-icon>
-          </button>
+        </button>
 
-
-          <!-- Topbar Navbar -->
-          <ul class="navbar-nav ml-auto">
-
-            <!-- Nav Item - Alerts -->
-            <li class="nav-item dropdown no-arrow mx-1">
-              <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-bell fa-fw"></i>
-                <!-- Counter - Alerts -->
-                <span class="badge badge-danger badge-counter">3+</span>
-              </a>
-            </li>
-
-            <!-- Nav Item - Messages -->
-            <li class="nav-item dropdown no-arrow mx-1">
-              <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-envelope fa-fw"></i>
-                <!-- Counter - Messages -->
-                <span class="badge badge-danger badge-counter">7</span>
-              </a>
-            </li>
-
-            <div class="topbar-divider d-none d-sm-block"></div>
+        <!-- Topbar Navbar -->
+        <ul class="navbar-nav ml-auto">
+            <div class="topbar-divider"></div>
 
             <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
-              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Valerie Luna</span>
-                <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
-              </a>
-              <!-- Dropdown - User Information -->
-              <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Profile
-                </a>
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Settings
-                </a>
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Activity Log
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                  <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Logout
-                </a>
-              </div>
+                <b-dropdown variant="link" right class="nav-link" toggle-class="pr-0">
+                    <template v-slot:button-content>
+                        <b-avatar size="large" class="mt-2"></b-avatar>
+                    </template>
+                    <b-dropdown-item to="/account">
+                        <b-icon icon="person-bounding-box" class="mr-1" /> 账户
+                    </b-dropdown-item>
+                    <b-dropdown-divider></b-dropdown-divider>
+                    <b-dropdown-item to="/login" @click="logout">
+                        <b-icon icon="box-arrow-right" class="mr-1" /> 登出
+                    </b-dropdown-item>
+                </b-dropdown>
             </li>
-
-          </ul>
-
-        </nav>
-        <!-- End of Topbar -->
+        </ul>
+    </nav>
+    <!-- End of Topbar -->
 </template>
 
 <script>
 export default {
-  props:['isToggled'],
-  methods: {
-    toggleSideBar() {
-      this.$emit('toggleSideBar')
-    }
-  }
-}
+    props: ['isToggled'],
+    methods: {
+        toggleSideBar() {
+            this.$emit('toggleSideBar');
+        },
+    },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
