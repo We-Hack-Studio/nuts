965f7b138ff28e851a3898bfe0303d861717a968
yangxg <zmrenwu@163.com>
2020-10-22 13:10:25 +0900

    improve api docs

--- yufuquant/exchanges/views.py
+++ yufuquant/exchanges/views.py
@@ -1,13 +1,20 @@
+from django.utils.decorators import method_decorator
+from drf_spectacular.utils import extend_schema
 from rest_framework import mixins, viewsets
 
 from .models import Exchange
 from .serializers import ExchangeSerializer
 
 
+@method_decorator(
+    name="list",
+    decorator=extend_schema(
+        summary="Return all exchanges",
+    ),
+)
 class ExchangeViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = ExchangeSerializer
     pagination_class = None
-    resource_name = "exchanges"
 
     def get_queryset(self):
         return Exchange.objects.all().order_by("-created_at")
