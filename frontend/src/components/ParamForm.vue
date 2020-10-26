27bbe1070053453c76ce337ea84e8f9b02a7e8e7
laomaner <1050397167@qq.com>
2020-10-26 17:29:41 +0900

    fix: input type

--- frontend/src/components/ParamForm.vue
+++ frontend/src/components/ParamForm.vue
@@ -1,66 +1,77 @@
 <template>
-  <b-form>
-    <b-form-group
-      v-for="field in fields"
-      :key="field.code"
-      :label="field.name"
-      :description="field.description"
-    >
-      <component
-        v-model="form[field.code]"
-        v-bind:is="fieldFactory(field.type)"
-        :items="field.items"
-      ></component>
-    </b-form-group>
-  </b-form>
+    <b-form>
+        <b-form-group v-for="field in fields" :key="field.code" :label="field.name" :description="field.description">
+            <component
+                v-if="getInputType(field) === 'number'"
+                v-model.number="form[field.code]"
+                v-bind:is="fieldFactory(field.type)"
+                :items="field.items"
+                type="number"
+            ></component>
+            <component
+                v-else
+                v-model="form[field.code]"
+                v-bind:is="fieldFactory(field.type)"
+                :items="field.items"
+            ></component>
+        </b-form-group>
+    </b-form>
 </template>
 
 <script>
-import { BFormInput } from "bootstrap-vue";
-import SelectItem from "./ParamFormSelectItem";
+import { BFormCheckbox, BFormInput } from 'bootstrap-vue';
+import SelectItem from './ParamFormSelectItem';
 
 export default {
-  name: "parameter-form",
-  props: {
-    fields: Array,
-  },
-  mounted() {
-    this.initForm();
-  },
-  data() {
-    return {
-      form: {},
-      strategyTemplateOptions: [],
-    };
-  },
-  components: { BFormInput },
+    name: 'parameter-form',
+    props: {
+        fields: Array,
+    },
+    mounted() {
+        this.initForm();
+    },
+    data() {
+        return {
+            form: {},
+            strategyTemplateOptions: [],
+        };
+    },
+    components: { BFormInput },
 
-  methods: {
-    initForm() {
-      this.fields.forEach((item) => {
-        this.$set(this.form, item.code, item.value);
-        // this.$set(this.form, item.code, 1);
-      });
+    methods: {
+        getInputType(field) {
+            if (field.type === 'float' || field.type === 'integer') {
+                return 'number';
+            }
+            return 'text';
+        },
+        initForm() {
+            this.fields.forEach(item => {
+                this.$set(this.form, item.code, item.value);
+                // this.$set(this.form, item.code, 1);
+            });
+        },
+        fieldFactory: function(fieldType) {
+            switch (fieldType) {
+                case 'boolean':
+                    return BFormCheckbox;
+                case 'integer':
+                case 'float':
+                    return BFormInput;
+                case 'enum':
+                    return SelectItem;
+                default:
+                    return BFormInput;
+            }
+        },
     },
-    fieldFactory: function (fieldType) {
-      switch (fieldType) {
-        case "integer":
-        case "float":
-          return BFormInput;
-        case "enum":
-          return SelectItem;
-        default:
-          return BFormInput;
-      }
-    },
-  },
 };
 </script>
 
 <style scoped>
 fieldset {
-  margin-bottom: 1rem;
-  padding: 0;
-  border: 0;
+    margin-bottom: 1rem;
+    padding: 0;
+    border: 0;
 }
 </style>
--- frontend/src/views/RobotView.vue
+++ frontend/src/views/RobotView.vue
@@ -1,166 +1,145 @@
 <template>
-  <b-row>
-    <b-col md="12">
-      <log-panel :log-list="logList"></log-panel>
-      <b-card header-tag="header" class="mt-3">
-        <template v-slot:header>
-          <div class="d-flex justify-content-between align-items-center">
-            <span class="h6">参数预览</span>
-            <a href="#" v-b-modal.param-form-modal>
-              <b-icon-pencil-square></b-icon-pencil-square>
-            </a>
-          </div>
-        </template>
-        <param-preview
-            :parameters="
-              strategySpecView && strategySpecView.parameters
-            "
-        ></param-preview>
-      </b-card>
-      <b-modal
-          button-size="sm"
-          size="md"
-          id="param-form-modal"
-          centered
-          title="参数设置"
-      >
-        <param-form
-            ref="paramForm"
-            :fields="
-              strategySpecView && strategySpecView.parameters
-            "
-        ></param-form>
-        <template v-slot:modal-footer="{ ok }">
-          <b-button size="sm" variant="primary" @click="onSubmit(ok, $event)"
-          >确认
-          </b-button
-          >
-        </template>
-      </b-modal>
-    </b-col>
-  </b-row>
+    <b-row class="m-1">
+        <b-col md="12">
+            <log-panel :log-list="logList"></log-panel>
+            <b-card header-tag="header" class="mt-3">
+                <template v-slot:header>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <span class="h6">参数预览</span>
+                        <a href="#" v-b-modal.param-form-modal>
+                            <b-icon-pencil-square></b-icon-pencil-square>
+                        </a>
+                    </div>
+                </template>
+                <param-preview :parameters="strategySpecView && strategySpecView.parameters"></param-preview>
+            </b-card>
+            <b-modal button-size="sm" size="md" id="param-form-modal" scrollable centered title="参数设置">
+                <param-form ref="paramForm" :fields="strategySpecView && strategySpecView.parameters"></param-form>
+                <template v-slot:modal-footer="{ ok }">
+                    <b-button size="sm" variant="primary" @click="onSubmit(ok, $event)">确认 </b-button>
+                </template>
+            </b-modal>
+        </b-col>
+    </b-row>
 </template>
 
 <script>
-import LogPanel from "./RobotLogView";
-import ParamPreview from "../components/ParamPreview";
-import ParamForm from "../components/ParamForm";
-import {getRobotsId} from "@/api";
-import {getRobotsIdStrategySpecView, patchRobotsIdStrategyParameters} from "@/api/robot";
+import LogPanel from './RobotLogView';
+import ParamPreview from '../components/ParamPreview';
+import ParamForm from '../components/ParamForm';
+import { getRobotsId } from '@/api';
+import { getRobotsIdStrategySpecView, patchRobotsIdStrategyParameters } from '@/api/robot';
 
 export default {
-  name: "robot-view",
-  data() {
-    return {
-      logList: [],
-      robotId: null,
-      strategySpecView: null,
-    };
-  },
-  components: {
-    LogPanel,
-    ParamPreview,
-    ParamForm,
-  },
-  methods: {
-    async onSubmit(ok, event) {
-      event.preventDefault();
-      let data = this.$refs.paramForm.form
-      try {
-        await patchRobotsIdStrategyParameters(
-            this.$route.params.id,
-            data
-        );
-        await this.getRobot();
-        this.$bvModal.hide("param-form-modal");
-      } catch (error) {
-        if (error.response) {
-          if (error.response.status === 500) {
-            this.$bvToast.toast(`服务端错误: ${error.response.status}`, {
-              title: "更新失败",
-              autoHideDelay: 3000,
-              toaster: "b-toaster-top-center",
-              variant: "danger",
-              appendToast: false,
-            });
-          } else if (error.response.status === 400) {
-            // this.paramFormErrors = error.response.data.errors;
-            // todo
-          } else {
-            this.$bvToast.toast(error.message, {
-              title: "数据获取失败",
-              autoHideDelay: 3000,
-              toaster: "b-toaster-top-center",
-              variant: "danger",
-              appendToast: false,
-            });
-          }
-        } else {
-          this.$bvToast.toast(error.message, {
-            title: "数据获取失败",
-            autoHideDelay: 3000,
-            toaster: "b-toaster-top-center",
-            variant: "danger",
-            appendToast: false,
-          });
-        }
-      }
+    name: 'robot-view',
+    data() {
+        return {
+            logList: [],
+            robotId: null,
+            strategySpecView: null,
+        };
     },
-    async getRobot() {
-      try {
-        const robotRes = await getRobotsId(this.$route.params.id);
-        const result = robotRes.data
-        this.robotId = result.id;
-      } catch (error) {
-        if (error.response) {
-          this.$bvToast.toast('机器人数据获取失败', {
-            title: '机器人数据获取失败',
-            autoHideDelay: 3000,
-            toaster: 'b-toaster-top-center',
-            variant: 'danger',
-            appendToast: false
-          });
-        } else {
-          this.$bvToast.toast(error.message, {
-            title: '机器人数据获取失败',
-            autoHideDelay: 3000,
-            toaster: 'b-toaster-top-center',
-            variant: 'danger',
-            appendToast: false
-          });
-        }
-      }
+    components: {
+        LogPanel,
+        ParamPreview,
+        ParamForm,
     },
+    methods: {
+        async onSubmit(ok, event) {
+            event.preventDefault();
+            let data = this.$refs.paramForm.form;
+            try {
+                await patchRobotsIdStrategyParameters(this.$route.params.id, data);
+                await this.getRobot();
+                this.$bvModal.hide('param-form-modal');
+            } catch (error) {
+                if (error.response) {
+                    if (error.response.status === 500) {
+                        this.$bvToast.toast(`服务端错误: ${error.response.status}`, {
+                            title: '更新失败',
+                            autoHideDelay: 3000,
+                            toaster: 'b-toaster-top-center',
+                            variant: 'danger',
+                            appendToast: false,
+                        });
+                    } else if (error.response.status === 400) {
+                        // this.paramFormErrors = error.response.data.errors;
+                        // todo
+                    } else {
+                        this.$bvToast.toast(error.message, {
+                            title: '数据获取失败',
+                            autoHideDelay: 3000,
+                            toaster: 'b-toaster-top-center',
+                            variant: 'danger',
+                            appendToast: false,
+                        });
+                    }
+                } else {
+                    this.$bvToast.toast(error.message, {
+                        title: '数据获取失败',
+                        autoHideDelay: 3000,
+                        toaster: 'b-toaster-top-center',
+                        variant: 'danger',
+                        appendToast: false,
+                    });
+                }
+            }
+        },
+        async getRobot() {
+            try {
+                const robotRes = await getRobotsId(this.$route.params.id);
+                const result = robotRes.data;
+                this.robotId = result.id;
+            } catch (error) {
+                if (error.response) {
+                    this.$bvToast.toast('机器人数据获取失败', {
+                        title: '机器人数据获取失败',
+                        autoHideDelay: 3000,
+                        toaster: 'b-toaster-top-center',
+                        variant: 'danger',
+                        appendToast: false,
+                    });
+                } else {
+                    this.$bvToast.toast(error.message, {
+                        title: '机器人数据获取失败',
+                        autoHideDelay: 3000,
+                        toaster: 'b-toaster-top-center',
+                        variant: 'danger',
+                        appendToast: false,
+                    });
+                }
+            }
+        },
 
-    async getStrategySpecView() {
-      try {
-        const response = await getRobotsIdStrategySpecView(this.$route.params.id);
-        this.strategySpecView = response.data;
-      } catch (error) {
-        if (error.response) {
-          this.$bvToast.toast('策略视图获取失败', {
-            title: '策略视图获取失败',
-            autoHideDelay: 3000,
-            toaster: 'b-toaster-top-center',
-            variant: 'danger',
-            appendToast: false
-          });
-        } else {
-          this.$bvToast.toast(error.message, {
-            title: '策略视图获取失败',
-            autoHideDelay: 3000,
-            toaster: 'b-toaster-top-center',
-            variant: 'danger',
-            appendToast: false
-          });
-        }
-      }
+        async getStrategySpecView() {
+            try {
+                const response = await getRobotsIdStrategySpecView(this.$route.params.id);
+                this.strategySpecView = response.data;
+            } catch (error) {
+                if (error.response) {
+                    this.$bvToast.toast('策略视图获取失败', {
+                        title: '策略视图获取失败',
+                        autoHideDelay: 3000,
+                        toaster: 'b-toaster-top-center',
+                        variant: 'danger',
+                        appendToast: false,
+                    });
+                } else {
+                    this.$bvToast.toast(error.message, {
+                        title: '策略视图获取失败',
+                        autoHideDelay: 3000,
+                        toaster: 'b-toaster-top-center',
+                        variant: 'danger',
+                        appendToast: false,
+                    });
+                }
+            }
+        },
     },
-  },
-  async mounted() {
-    await this.getRobot();
-    await this.getStrategySpecView()
-  },
+    async mounted() {
+        await this.getRobot();
+        await this.getStrategySpecView();
+    },
 };
 </script>
 
