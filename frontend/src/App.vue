363b34a9f3863a0f4b802c7264a30ff41fa179de
laomaner <1050397167@qq.com>
2020-10-28 23:56:30 +0900

    fix: sidebar collapse

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,50 +1,50 @@
 <template>
-  <div id="app">
-    <side-bar @toggleSideBar="handleToggleSideBar" :isToggled="isToggled"></side-bar>
-    <div class="content-wrapper">
-      <top-bar class="mb-4" @toggleSideBar="handleToggleSideBar" :isToggled="isToggled"></top-bar>
-      <router-view></router-view>
+    <div id="app">
+        <side-bar @toggleSideBar="handleToggleSideBar" :isToggled="isToggled"></side-bar>
+        <div class="content-wrapper">
+            <top-bar class="mb-4" @toggleSideBar="handleToggleSideBar" :isToggled="isToggled"></top-bar>
+            <router-view></router-view>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-    import TopBar from "./components/TopBar";
-    import SideBar from './components/SideBar';
+import TopBar from './components/TopBar';
+import SideBar from './components/SideBar';
 
-    export default {
-        name: 'App',
-          data() {
-            return {
-              isToggled: false,
+export default {
+    name: 'App',
+    data() {
+        return {
+            isToggled: true,
+        };
+    },
+    mounted() {
+        window.onresize = () => {
+            const width = document.documentElement.clientWidth;
+            if (width > 576) {
+                this.isToggled = false;
             }
-          },
-        mounted() {
-          window.onresize = () => {
-              const width =document.documentElement.clientWidth; 
-              if (width < 576) {
-                this.isToggled = true
-              }
-          }
+        };
+    },
+    components: {
+        TopBar,
+        SideBar,
+    },
+    methods: {
+        handleToggleSideBar() {
+            this.isToggled = !this.isToggled;
         },
-        components: {
-            TopBar,
-            SideBar
-        },
-        methods: {
-            handleToggleSideBar() {
-              this.isToggled = !this.isToggled;
-            }
-        }
-    }
+    },
+};
 </script>
 
 <style lang="scss">
 @import './assets/nav.scss';
 #app {
-  display: flex;
+    display: flex;
 }
 .content-wrapper {
-  width: 100%;
+    width: 100%;
 }
 </style>
